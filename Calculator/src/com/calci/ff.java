c10906dcd724e0c97610bd4356ef8d424eeb4ca0
om <om@DESKTOP-5D2KI06>
2023-09-16 12:01:01 +0530

    dddd

---
+++ Calculator/src/com/calci/ff.java
@@ -0,0 +1,43 @@
+package com.calci;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class ff {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ff window = new ff();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ff() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
